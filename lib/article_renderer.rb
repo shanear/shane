86d20c9c2a4625992db981bc1d98c926d388e82e
Shane Russell <shane1337@gmail.com>
2013-04-15 18:05:48 -0700

    Blog images and formatting updates

--- lib/article_renderer.rb
+++ lib/article_renderer.rb
@@ -1,5 +1,10 @@
 class ArticleRenderer < Redcarpet::Render::HTML
   def image(link, title, alt_text)
-    "<img src='#{link}' alt='#{alt_text}/><div class='caption'>#{alt_text}</div>"
+    <<-HTML
+      <div class='image'>
+        <img src='/assets/blog/#{link}' alt='#{alt_text}'/>
+        <div class='caption'>#{alt_text}</div>
+      </div>
+    HTML
   end
 end
